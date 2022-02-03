c3d231358a2b95f4da28b49c0c8f2dbab4ce7cea
jmarciel <jmarciel@everis.com>
2022-02-03 14:15:40 +0100

    initial commit

---
+++ WhatsDown/WhatsDown/WhatsDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsDownApp.swift
+//  WhatsDown
+//
+//  Created by Jorge Marciel Pariente on 3/2/22.
+//
+
+import SwiftUI
+
+@main
+struct WhatsDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
